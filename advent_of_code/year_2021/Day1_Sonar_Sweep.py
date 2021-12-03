c2d6e06ce640bdb586490bef8df5b065820dbb03
_l3moN <l3mon.off@gmail.com>
2021-12-03 02:46:45 +0100

    edited day 1 by format I will use later

--- advent_of_code/year_2021/Day1_Sonar_Sweep.py
+++ advent_of_code/year_2021/Day1_Sonar_Sweep.py
@@ -1,23 +1,22 @@
-with open(r'txt_files/day1.txt') as f:
-    data = f.read().splitlines()
-    data = list(map(int, data))
+if __name__ == '__main__':
+    with open(r'txt_files/day1.txt') as f:
+        data = f.read().splitlines()
+        data = list(map(int, data))
 
+    def first_star():
+        count = 0
+        for i in range(len(data)-1):
+            if data[i] < data[i+1]:
+                count += 1
+        # print(data)
+        print(count)
+    first_star()
 
-def first_star():
-    count = 0
-    for i in range(len(data)-1):
-        if data[i] < data[i+1]:
-            count += 1
-    # print(data)
-    print(count)
-first_star()
-
-
-def second_star():
-    count = 0
-    for i in range(len(data)-3):
-        # print(data[i:i+3])
-        if sum(data[i:i+3]) < sum(data[i+1:i+4]):
-            count += 1
-    print(count)
-second_star()
+    def second_star():
+        count = 0
+        for i in range(len(data)-3):
+            # print(data[i:i+3])
+            if sum(data[i:i+3]) < sum(data[i+1:i+4]):
+                count += 1
+        print(count)
+    second_star()
