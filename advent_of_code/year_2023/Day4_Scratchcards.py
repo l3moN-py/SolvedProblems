f57f51212f26b39b87e977b054a3afb8b5ff7c7f
Martin Klacik <klacikmartin@protonmail.com>
2023-12-09 20:04:59 +0100

    day4 part two

--- advent_of_code/year_2023/Day4_Scratchcards.py
+++ advent_of_code/year_2023/Day4_Scratchcards.py
@@ -1,7 +1,19 @@
-import math
-
 from script import loadExample, loadTxt
-import re
+
+
+def calcScratchcards(cardDeck):
+    cardsNumber = [1 for _ in range(len(cardDeck))]
+
+    for i in range(len(cardDeck)):
+        cardPack = cardDeck[i]
+        winningCards, elfsCard = cardPack
+        winningTimes = 0
+        for winningCard in winningCards:
+            if winningCard in elfsCard:
+                winningTimes += 1
+        cardsNumber = cardsNumber[:i+1] + list(map(lambda x: x+cardsNumber[i], cardsNumber[i+1:i+1+winningTimes])) + cardsNumber[i+1+winningTimes:]
+
+    return sum(cardsNumber)
 
 
 def calcWinning(cardDeck):
@@ -16,18 +28,26 @@ def calcWinning(cardDeck):
     return 0 if count == 0 else 2**(count-1)
 
 
+def processCards(cards):
+    cards = [card.split(": ")[1].split(" | ") for card in cards]
+    cards = [[set(card[0].split()), set(card[1].split())] for card in cards]
+    return cards
+
+
 def partOne():
     cards = loadExample()
     cards = loadTxt("day4")
-    cards = [card.split(": ")[1].split(" | ") for card in cards]
-    cards = [[set(card[0].split()), set(card[1].split())] for card in cards]
+    cards = processCards(cards)
     cardsWinningSum = sum([calcWinning(card) for card in cards])
     return cardsWinningSum
 
 
 def partTwo():
+    cards = loadExample()
+    cards = loadTxt("day4")
+    cards = processCards(cards)
 
-    return -1
+    return calcScratchcards(cards)
 
 
 if __name__ == "__main__":
